4583392bf782338c8407aa3c43c7e5766d9044f8
JF-117 <121064965+JF-117@users.noreply.github.com>
2023-09-21 15:25:20 -0400

    Added UI with Basket Lives

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // This line enables use of uGUI features. // a
 
 public class Basket : MonoBehaviour {
+[Header("Set Dynamically")]
+
+public Text scoreGT; // a
+void Start() {
+// Find a reference to the ScoreCounter GameObject
+GameObject scoreGO = GameObject.Find("ScoreCounter"); // b
+// Get the Text Component of that GameObject
+scoreGT = scoreGO.GetComponent<Text>(); // c
+// Set the starting number of points to 0
+scoreGT.text = "0";
+}
 
 void Update () {
 // Get the current screen position of the mouse from Input
@@ -25,6 +37,18 @@ void OnCollisionEnter( Collision coll ) { // a
 GameObject collidedWith = coll.gameObject; // b
 if ( collidedWith.tag == "Apple" ) { // c
 Destroy( collidedWith );
+
+// Parse the text of the scoreGT into an int
+int score = int.Parse( scoreGT.text ); // d
+// Add points for catching the apple
+score += 100;
+// Convert the score back to a string and display it
+scoreGT.text = score.ToString();
+
+// Track the high score
+if (score > HighScore.score) {
+HighScore.score = score;
+}
 }
 }
 }
