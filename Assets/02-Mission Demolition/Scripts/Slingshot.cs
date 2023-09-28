5ce46caea95a3f1921932000c32b1a471d241598
JF-117 <121064965+JF-117@users.noreply.github.com>
2023-09-28 02:06:09 -0400

    FollowCam Started work on Cloud

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -38,10 +38,13 @@ public class Slingshot : MonoBehaviour
     void OnMouseDown() { // d
     // The player has pressed the mouse button while over Slingshot
     aimingMode = true;
+
     // Instantiate a Projectile
     projectile = Instantiate( prefabProjectile ) as GameObject;
+
     // Start it at the launchPoint
     projectile.transform.position = launchPos;
+
     // Set it to isKinematic for now
     projectileRigidbody = projectile.GetComponent<Rigidbody>(); // a
     projectileRigidbody.isKinematic = true; // a
@@ -50,26 +53,32 @@ public class Slingshot : MonoBehaviour
     void Update() {
     // If Slingshot is not in aimingMode, don't run this code
     if (!aimingMode) return; // b
+
     // Get the current mouse position in 2D screen coordinates
     Vector3 mousePos2D = Input.mousePosition; // c
     mousePos2D.z = -Camera.main.transform.position.z;
     Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+
     // Find the delta from the launchPos to the mousePos3D
     Vector3 mouseDelta = mousePos3D-launchPos;
+
     // Limit mouseDelta to the radius of the Slingshot SphereCollider // d
     float maxMagnitude = this.GetComponent<SphereCollider>().radius;
     if (mouseDelta.magnitude > maxMagnitude) {
     mouseDelta.Normalize();
     mouseDelta *= maxMagnitude;
     }
+
     // Move the projectile to this new position
     Vector3 projPos = launchPos + mouseDelta;
     projectile.transform.position = projPos;
+
     if ( Input.GetMouseButtonUp(0) ) { // e
     // The mouse has been released
     aimingMode = false;
     projectileRigidbody.isKinematic = false;
     projectileRigidbody.velocity = -mouseDelta * velocityMult;
+    FollowCam.POI = projectile;
     projectile = null;
     }
     }
